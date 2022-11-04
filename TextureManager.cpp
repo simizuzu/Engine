035e7fc1df1ed66783e5e6a46eb7ed18694468ef
翔 清水 <k021g1379@g.neec.ac.jp>
2022-11-05 03:33:16 +0900

    テクスチャマネージャ作った

--- TextureManager.cpp
+++ TextureManager.cpp
@@ -1,1 +1,10 @@
 #include "TextureManager.h"
+
+void TextureManager::Initialize()
+{
+	// デスクリプタヒープの設定
+	D3D12_DESCRIPTOR_HEAP_DESC srvHeapDesc = {};
+	srvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+	srvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;//シェーダから見えるように
+	srvHeapDesc.NumDescriptors = MaxSRVCount;
+}
--- TextureManager.h
+++ TextureManager.h
@@ -1,5 +1,24 @@
 #pragma once
+
+#include <d3d12.h>
+#include <wrl.h>
+
+/// <summary>
+/// テクスチャマネージャ
+/// </summary>
 class TextureManager
 {
+public: // エイリアス
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+
+public: // 定数
+	static const int MaxSRVCount = 512; // テクスチャの最大枚数
+
+public: // メンバ関数
+	void Initialize();
+
+private: // メンバ変数
+	ComPtr<ID3D12DescriptorHeap> srvHeap;
+	ComPtr<ID3D12Resource> spriteTexBuff[MaxSRVCount];
 };
 

8aa6aef138e867e81c33166c9b8fcb4d9a31f5f9
翔 清水 <k021g1379@g.neec.ac.jp>
2022-11-19 23:45:51 +0900

    シェーダー読み込みをクラス化した

--- lib/engine/2D/Sprite/Sprite.cpp
+++ lib/engine/2D/Sprite/Sprite.cpp
@@ -39,61 +39,7 @@ void Sprite::StaticInitialize()
 	// 平行投影
 	MyMathUtility::MakeOrthogonalL(0.0f, width, height, 0.0f, 0.0f, 1.0f, matProjection_);
 
-	//// エラーオブジェクト
-	//ComPtr<ID3DBlob> errorBlob;
-
-	////　頂点シェーダの読み込みとコンパイル
-	//HRESULT result = D3DCompileFromFile(
-	//	L"Resources/shaders/SpriteVS.hlsl",	// シェーダファイル名
-	//	nullptr,
-	//	D3D_COMPILE_STANDARD_FILE_INCLUDE,	// インクルード可能にする
-	//	"main", "vs_5_0",	// エントリーポイント名、シェーダモデル指定
-	//	D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバック用設定
-	//	0, &vsBlob, &errorBlob
-	//);
-
-	//// シェーダのエラー内容を表示
-	//if (FAILED(result))
-	//{
-	//	// errorBlobからエラー内容をstring型にコピー
-	//	std::string error;
-	//	error.resize(errorBlob->GetBufferSize());
-
-	//	std::copy_n((char*)errorBlob->GetBufferPointer(),
-	//		errorBlob->GetBufferSize(),
-	//		error.begin());
-	//	error += "\n";
-	//	// エラー内容を出力ウィンドウに表示
-	//	OutputDebugStringA(error.c_str());
-	//	assert(0);
-	//}
-
-	//// ピクセルシェーダの読み込みとコンパイル
-	//result = D3DCompileFromFile(
-	//	L"Resources/shaders/SpritePS.hlsl",	// シェーダファイル名
-	//	nullptr,
-	//	D3D_COMPILE_STANDARD_FILE_INCLUDE,	// インクルード可能にする
-	//	"main", "ps_5_0",	// エンドリーポイント名、シェーダモデル指定
-	//	D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION, // デバック用設定
-	//	0, &psBlob, &errorBlob
-	//);
-
-	//// エラーなら
-	//if (FAILED(result))
-	//{
-	//	// errorBlobからエラー内容をstring型にコピー
-	//	std::string error;
-	//	error.resize(errorBlob->GetBufferSize());
-
-	//	std::copy_n((char*)errorBlob->GetBufferPointer(),
-	//		errorBlob->GetBufferSize(),
-	//		error.begin());
-	//	error += "\n";
-	//	// エラー内容を出力ウィンドウに表示
-	//	OutputDebugStringA(error.c_str());
-	//	assert(0);
-	//}
-
+	// シェーダーの読み込み
 	shader_->CreateSpriteShade(vsBlob, psBlob);
 
 	for (int i = 0; i < pipelineState.size(); i++)

25f5926ff9472983c21b2e3a8224dd86e2ea46f9
ShoShimizu <k021g1379@g.neec.ac.jp>
2023-05-26 15:59:59 +0900

    totyuu

---
+++ PostEffect.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Sprite.h"
+class PostEffect : public Sprite
+{
+public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
+	PostEffect();
+
+	/// <summary>
+	/// 描画コマンドの実行
+	/// </summary>
+	/// <param name="cmdList">コマンドリスト</param>
+	void Draw(ID3D12GraphicsCommandList* cmdList);
+};
+

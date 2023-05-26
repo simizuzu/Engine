07dea132a48db19e460fa5cce9c81c756bc78417
ShoShimizu <k021g1379@g.neec.ac.jp>
2023-05-26 16:00:17 +0900

    add

--- PostEffect.h
+++ PostEffect.h
@@ -1,6 +1,20 @@
 #pragma once
-#include "Sprite.h"
-class PostEffect : public Sprite
+#include <DirectXMath.h>
+#include <memory>
+#include <wrl.h>
+
+#include "DirectX12Math.h"
+#include "TextureManager.h"
+#include "DirectXCommon.h"
+#include "WinApp.h"
+#include "Shader.h"
+#include "Pipeline.h"
+#include "Vector2.h"
+#include "Vector3.h"
+#include "Vector4.h"
+#include "Matrix4.h"
+
+class PostEffect
 {
 public:
 	/// <summary>
